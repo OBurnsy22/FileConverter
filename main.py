b15c5e40be5b96305e1d017e00062a87da858ee4
OBurnsy22 <oburnsy22@gmail.com>
2021-03-08 21:20:14 -0800

    Re-arranged widgets, drag and drop box now takes up entire container window

--- main.py
+++ main.py
@@ -41,13 +41,33 @@ def retrieve_compatable_conversions(file_type):
         return images
 
 #container layouts
-dropbox_layout = tk.Frame(root, bg='green', width=700, height=520)
-conversion_layout = tk.Frame(root, bg='yellow', width=230, height=520)
+dropbox_layout = tk.Frame(root, bg='#DCDCDC', width=690, height=520)
+conversion_layout = tk.Frame(root, bg='#DCDCDC', width=240, height=520)
 
-#layout main containers
+#size containers
 root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
-dropbox_layout.grid(column=0, row=0,pady=10)
-conversion_layout.grid(column=1, row=0, padx=10)
+dropbox_layout.grid(column=0, row=0,pady=10, sticky="nsew")
+conversion_layout.grid(column=1, row=0, padx=10, sticky="nsew")
+dropbox_layout.rowconfigure(0, weight=1)
+dropbox_layout.columnconfigure(0, weight=1)
+
+#declaring widgets for dropbox_layout
+dropbox = tk.Listbox(dropbox_layout, selectmode=tk.SINGLE, background="#99ff99")
+dropbox.drop_target_register(DND_FILES)
+dropbox.dnd_bind("<<Drop>>", open_dropped_file)
+select_file = tk.Button(dropbox_layout, text="Select File", command=open_selected_file)
+text_label = tk.Label(dropbox_layout, text="Drag and Drop or Select a File")
+canvas = tk.Canvas(dropbox_layout, width=100, height=100)
+img = tk.PhotoImage(file="static/drag_and_drop.png") #convert this to gif when you come back
+
+#placing widgets for dropbox_layout
+dropbox.grid(column=0, row=0, sticky="nesw")
+select_file.grid(column=0, row=0)
+canvas.grid(column=0, row=0)
+canvas.create_image(20, 20, anchor=NW, image=img)
+#text_label.grid(column=0, row=0, pady=(50))
+#widgets for conversion_layout
+
 #runs application
 root.mainloop()
